4bed64b776e8bd0555d5293046a44f27f4849e80
Dexter Chua <dalcde@yahoo.com.hk>
2019-07-30 14:09:48 -0400

    Use WebWorker

--- js/index.js
+++ js/index.js
@@ -1,18 +1,18 @@
 import "spectral-sequences";
 
-import("../pkg/index.js").catch(console.error).then(wasm => {
-    window.sseq = new Sseq();
-    let display = new BasicDisplay("#main", sseq);
-    // Worker("worker.js");
-    let C2json = '{"name": "$C(2)$", "file_name": "C2", "p": 2, "generic": false, "gens": {"x0": 0, "x1": 1}, "sq_actions": [{"op": 1, "input": "x0", "output": [{"gen": "x1", "coeff": 1}]}], "adem_actions": [{"op": [1], "input": "x0", "output": [{"gen": "x1", "coeff": 1}]}], "milnor_actions": [{"op": [1], "input": "x0", "output": [{"gen": "x1", "coeff": 1}]}]}';
-    let max_degree = 30;
-    let add_class = (a, b, name) => {
-        sseq.addClass(b-a,a);
-    };
-    // let add_structline = ()
-    wasm.resolve(C2json, max_degree, add_class);
-});
+window.sseq = new Sseq();
+window.display = new BasicDisplay("#main");
+display.setSseq(sseq);
 
-// import("../crate/pkg").then(module => {
-    //   });
-//     module.run();
+const worker = new Worker("./worker.js");
+
+worker.addEventListener("message", ev => {
+    let m = ev.data;
+    switch (m.cmd) {
+        case "addClass":
+            sseq.addClass(m.x, m.y)
+            break;
+        default:
+            break;
+    }
+});
--- webpack.config.js
+++ webpack.config.js
@@ -4,26 +4,44 @@ const WasmPackPlugin = require("@wasm-tool/wasm-pack-plugin");
 
 const dist = path.resolve(__dirname, "dist");
 
-module.exports = {
-  mode: "production",
-  entry: {
-    index: "./js/index.js"
-  },
-  output: {
-    path: dist,
-    filename: "[name].js"
-  },
-  devServer: {
-    contentBase: dist,
-  },
-  plugins: [
-    new CopyPlugin([
-      path.resolve(__dirname, "static")
-    ]),
-
-    new WasmPackPlugin({
-      crateDirectory: __dirname,
-      extraArgs: "--out-name index"
-    }),
-  ]
+const appConfig = {
+    mode: "production",
+    entry: {
+        index: "./js/index.js"
+    },
+    devServer: {
+        contentBase: dist
+    },
+    resolve: {
+        extensions: [".js"]
+    },
+    plugins: [
+        new CopyPlugin([
+            path.resolve(__dirname, "static")
+        ]),
+    ],
+    output: {
+        path: dist,
+        filename: "[name].js"
+    }
 };
+
+const workerConfig = {
+    entry: "./js/worker.js",
+    target: "webworker",
+    plugins: [
+        new WasmPackPlugin({
+            crateDirectory: __dirname,
+            extraArgs: "--out-name index"
+        })
+    ],
+    resolve: {
+        extensions: [".js", ".wasm"]
+    },
+    output: {
+        path: dist,
+        filename: "worker.js"
+    }
+}
+
+module.exports = [appConfig, workerConfig]
