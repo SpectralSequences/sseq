cb62c17779327f6be9c7db43f7bd42310d5ea7da
Dexter Chua <dalcde@yahoo.com.hk>
2019-08-03 16:37:53 -0400

    Bold rust output

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 extern crate rust_ext;
+extern crate ansi_term;
 
 use rust_ext::Config;
 use rust_ext::run;
@@ -14,7 +15,7 @@ fn main() {
     });
 
     match run(config) {
-        Ok(string) => println!("{}", string),
+        Ok(string) => println!("{}", ansi_term::Style::new().bold().paint(string)),
         Err(e) => { eprintln!("Application error: {}", e); std::process::exit(1); }
     }
 }
