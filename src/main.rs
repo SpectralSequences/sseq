5dff285cfca7be739d3066d3dcecf9db1c6c5c62
Dexter Chua <dalcde@yahoo.com.hk>
2019-08-03 10:45:39 -0400

    Allow choice of algebra in command line

--- src/main.rs
+++ src/main.rs
@@ -28,7 +28,6 @@ use serde_json::value::Value;
 
 use crate::algebra::Algebra;
 use crate::adem_algebra::AdemAlgebra;
-#[cfg(test)]
 use crate::milnor_algebra::MilnorAlgebra;
 use crate::finite_dimensional_module::FiniteDimensionalModule;
 use crate::chain_complex::ChainComplexConcentratedInDegreeZero;
@@ -46,47 +45,73 @@ fn main() {
         std::process::exit(1);
     });
 
-    if let Err(e) = run(config) {
-        eprintln!("Application error: {}", e);
-        std::process::exit(1);
+    match run(config) {
+        Ok(string) => println!("{}", string),
+        Err(e) => { eprintln!("Application error: {}", e); std::process::exit(1); }
     }
 }
 
 #[allow(non_snake_case)]
-fn run(config : Config) -> Result<(), Box<Error>> {
+fn run(config : Config) -> Result<String, Box<Error>> {
     let contents = std::fs::read_to_string(format!("static/modules/{}.json", config.module_name))?;
     let mut json : Value = serde_json::from_str(&contents)?;
     let p = json["p"].as_u64().unwrap() as u32;
     let max_degree = config.max_degree;
 
-    let A = AdemAlgebra::new(p, p != 2, false, max_degree);
+    // You need a box in order to allow for different possible types implementing the same
+    // trait
+    let A : Box<Algebra>;
+    match config.algebra_name.as_ref() {
+        "adem" => A = Box::new(AdemAlgebra::new(p, p != 2, false, max_degree)),
+        "milnor" => A = Box::new(MilnorAlgebra::new(p)),
+        _ => { println!("Invalid algebra"); return Err(Box::new(InvalidAlgebraError { name : config.algebra_name })); }
+    };
+
     A.compute_basis(max_degree);
-    let M = FiniteDimensionalModule::from_json(&A, "adem", &mut json);
+    let M = FiniteDimensionalModule::from_json(&*A, &config.algebra_name, &mut json);
     let CC = ChainComplexConcentratedInDegreeZero::new(&M);
     let res = Resolution::new(&CC, max_degree, None, None);
     res.resolve_through_degree(max_degree);
-    println!("{}", res.graded_dimension_string());
-    Ok(())
+    Ok(res.graded_dimension_string())
 }
 
 struct Config {
     module_name : String,
+    algebra_name : String,
     max_degree : i32
 }
 
 impl Config {
     fn new(args: &[String]) -> Result<Self, String> {
-        if args.len() < 3 {
+        if args.len() < 4 {
             return Err("Not enough arguments".to_string());
         }
         let module_name = args[1].clone();
-        let max_deg_result : Result<i32,_> = args[2].parse();
+        let algebra_name = args[2].clone();
+        let max_deg_result : Result<i32,_> = args[3].parse();
 
         if let Err(error) = max_deg_result {
             return Err(format!("{} in argument max_degree.", error));
         }
         let max_degree = max_deg_result.unwrap();
-        Ok(Self { module_name, max_degree })
+        Ok(Self { module_name, algebra_name, max_degree })
+    }
+}
+
+#[derive(Debug)]
+struct InvalidAlgebraError {
+    name : String
+}
+
+impl std::fmt::Display for InvalidAlgebraError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "Invalid algebra: {}", &self.name)
+    }
+}
+
+impl Error for InvalidAlgebraError {
+    fn description(&self) -> &str {
+        "Invalid algebra supplied"
     }
 }
 
@@ -107,38 +132,22 @@ mod tests {
         compare("C3", 60);
     }
 
-    fn compare(filename : &str, max_degree : i32) {
-        let contents = std::fs::read_to_string(format!("static/modules/{}.json", filename)).unwrap();
+    fn compare(module_name : &str, max_degree : i32) {
+        let a = Config {
+            module_name : String::from(module_name),
+            max_degree,
+            algebra_name : String::from("adem")
+        };
+        let m = Config {
+            module_name : String::from(module_name),
+            max_degree,
+            algebra_name : String::from("milnor")
+        };
 
-        assert_eq!(run_adem(&contents, max_degree), run_milnor(&contents, max_degree));
+        match (run(a), run(m)) {
+            (Err(e), _)    => panic!("Failed to read file: {}", e),
+            (_, Err(e))    => panic!("Failed to read file: {}", e),
+            (Ok(x), Ok(y)) => assert_eq!(x, y)
+        }
     }
-
-    fn run_adem(contents : &str, max_degree : i32) -> String {
-        let mut json : Value = serde_json::from_str(contents).unwrap();
-        let p = json["p"].as_u64().unwrap() as u32;
-
-        let A = AdemAlgebra::new(p, p != 2, false, max_degree);
-        A.compute_basis(max_degree);
-        let M = FiniteDimensionalModule::from_json(&A, "adem", &mut json);
-        let CC = ChainComplexConcentratedInDegreeZero::new(&M);
-        let res = Resolution::new(&CC, max_degree, None, None);
-        res.resolve_through_degree(max_degree);
-
-        res.graded_dimension_string()
-    }
-
-    fn run_milnor(contents : &str, max_degree : i32) -> String {
-        let mut json : Value = serde_json::from_str(contents).unwrap();
-        let p = json["p"].as_u64().unwrap() as u32;
-
-        let A = MilnorAlgebra::new(p);
-        A.compute_basis(max_degree);
-        let M = FiniteDimensionalModule::from_json(&A, "milnor", &mut json);
-        let CC = ChainComplexConcentratedInDegreeZero::new(&M);
-        let res = Resolution::new(&CC, max_degree, None, None);
-        res.resolve_through_degree(max_degree);
-
-        res.graded_dimension_string()
-    }
-
 }
