6f8a6f943ad6f76b397e63ce2823e43a3a931541
Dexter Chua <dalcde@yahoo.com.hk>
2019-08-06 10:21:12 -0400

    Use tests directory instead of putting in main.rs

---
+++ tests/milnor_vs_adem.rs
@@ -0,0 +1,34 @@
+use rust_ext::Config;
+use rust_ext::run;
+
+#[test]
+fn milnor_vs_adem() {
+    compare("S_2", 30);
+    compare("C2", 30);
+    compare("Joker", 30);
+    compare("RP4", 30);
+    compare("Csigma", 30);
+    compare("S_3", 30);
+    compare("Calpha", 30);
+    compare("C3", 60);
+}
+
+fn compare(module_name : &str, max_degree : i32) {
+    println!("module : {}", module_name);
+    let a = Config {
+        module_path : format!("static/modules/{}.json", module_name),
+        max_degree,
+        algebra_name : String::from("adem")
+    };
+    let m = Config {
+        module_path : format!("static/modules/{}.json", module_name),
+        max_degree,
+        algebra_name : String::from("milnor")
+    };
+
+    match (run(&a), run(&m)) {
+        (Err(e), _)    => panic!("Failed to read file: {}", e),
+        (_, Err(e))    => panic!("Failed to read file: {}", e),
+        (Ok(x), Ok(y)) => assert_eq!(x, y)
+    }
+}
