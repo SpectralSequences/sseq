fc266e9cab51890697f69a8edf59f20b603d9a27
Dexter Chua <dalcde@yahoo.com.hk>
2019-09-21 10:58:31 -0400

    Implement Milnor basis coproduct
    This is pretty slow compared to the Adem version, but lets us compute
    all the Steenrod operations on tmf:
    
    Sq*(h_0) = (h_0^2, h_1)
    Sq*(h_1) = (h_1^2, h_2)
    Sq*(h_2) = (h_2^2, 0)
    
    Sq*(c_0) = (0, h_0 e_0, h_2 \beta, 0)
    Sq*(\alpha) = (\alpha^2, \gamma, 0, 0)
    Sq*(\beta) = (\beta^2, 0, 0, 0)
    
    Sq*(w_1) = (w_1^2, 0, 0, 0, g)
    Sq*(d_0) = (d_0^2, 0, \beta^2, 0, 0)
    Sq*(e_0) = (e_0^2, \beta g, 0, 0, 0)
    Sq*(g) = (g^2, 0, 0, 0, 0)
    
    Sq*(\gamma) = (\gamma^2, h_2 w_2, 0, 0, 0, 0)
    
    Sq*(\delta) = (0, h_2 w_2 d_0, w_2 h_2 \beta, 0, 0, 0, 0, 0)
    
    Sq*(w_2) = (w_2^2, 0, 0, 0, 0, 0, 0, 0, 0)

--- src/algebra/milnor_algebra.rs
+++ src/algebra/milnor_algebra.rs
@@ -135,9 +135,7 @@ impl MilnorAlgebra {
         if let Some(idx) = self.basis_element_to_index_map[elt.degree as usize].get(elt) {
             *idx
         } else {
-            println!("Didn't find element: {:?}", elt);
-            assert!(false);
-            0
+            panic!("Didn't find element: {:?}", elt)
         }
     }
 }
@@ -987,12 +985,76 @@ mod tests {
         }
     }    
 }
+
+impl MilnorAlgebra {
+    /// Returns `true` if the new element is not within the bounds
+    fn increment_p_part(element: &mut Vec<u32>, max : &Vec<u32>) -> bool {
+        element[0] += 1;
+        for i in 0 .. element.len() - 1{
+            if element[i] > max[i] {
+                element[i] = 0;
+                element[i + 1] += 1;
+            }
+        }
+        element.last().unwrap() > max.last().unwrap()
+    }
+}
+
 impl Bialgebra for MilnorAlgebra {
     fn coproduct(&self, op_deg : i32, op_idx : usize) -> Vec<(i32, usize, i32, usize)> {
-        unimplemented!()
+        assert_eq!(self.prime(), 2, "Coproduct at odd primes not supported");
+        if op_deg == 0 {
+            return vec![(0, 0, 0, 0)];
+        }
+        let xi_degrees = combinatorics::xi_degrees(self.p);
+
+        let mut len = 1;
+        let p_part = &self.basis_element_from_index(op_deg, op_idx).p_part;
+
+        for i in p_part.iter() {
+            len *= i + 1;
+        }
+        let len = len as usize;
+        let mut result = Vec::with_capacity(len);
+
+        let mut cur_ppart : Vec<u32> = vec![0; p_part.len()];
+        loop {
+            let mut left_degree : i32 = 0;
+            for i in 0 .. cur_ppart.len() {
+                left_degree += cur_ppart[i] as i32 * xi_degrees[i];
+            }
+            let right_degree : i32 = op_deg - left_degree;
+
+            let mut left_ppart = cur_ppart.clone();
+            while let Some(0) = left_ppart.last() {
+                left_ppart.pop();
+            }
+
+            let mut right_ppart = cur_ppart.iter().enumerate().map(|(i, v)| p_part[i] - *v).collect::<Vec<_>>();
+            while let Some(0) = right_ppart.last() {
+                right_ppart.pop();
+            }
+
+            let left_idx = self.basis_element_to_index(&MilnorBasisElement {
+                    degree : left_degree,
+                    q_part : 0,
+                    p_part : left_ppart
+                });
+            let right_idx = self.basis_element_to_index(&MilnorBasisElement {
+                    degree: right_degree,
+                    q_part : 0,
+                    p_part : right_ppart
+                });
+
+            result.push((left_degree, left_idx, right_degree, right_idx));
+            if Self::increment_p_part(&mut cur_ppart, p_part) {
+                break;
+            }
+        }
+        result
     }
     fn decompose(&self, op_deg : i32, op_idx : usize) -> Vec<(i32, usize)> {
-        unimplemented!()
+        vec![(op_deg, op_idx)]
     }
 
 }
