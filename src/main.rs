f3bc22f287294b00d10f8321b0704aee802c0604
Dexter Chua <dalcde@yahoo.com.hk>
2019-12-22 19:32:26 -0500

    Remove final unused extern crate

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,10 @@
-extern crate rust_ext;
-#[macro_use]
-extern crate clap;
-
 use rust_ext::Config;
 use rust_ext::run_resolve;
 use rust_ext::run_define_module;
 use rust_ext::run_test;
 use rust_ext::run_yoneda;
 use rust_ext::run_steenrod;
-use clap::App;
+use clap::{App, load_yaml, value_t};
 
 const BOLD_ANSI_CODE : &str = "\x1b[1m";
 
