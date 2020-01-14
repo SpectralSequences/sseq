551183b4364443553a8327f5d6a0490f71d6227f
Dexter Chua <dexter@math.harvard.edu>
2020-01-14 13:21:53 +0800

    Fix rpn calculation
    This requires rust 1.38 or above.

--- src/module/rpn.rs
+++ src/module/rpn.rs
@@ -161,7 +161,7 @@ impl RealProjectiveSpace {
         let clear_bottom = spec.clear_bottom.unwrap_or(false);
         let mut min = spec.min;
         if clear_bottom {
-            let x = (spec.min + 1) % 8; //.rem_euclid(8);
+            let x = (spec.min + 1).rem_euclid(8);
             if x != 0 {
                 min += 8 - x;
             }
