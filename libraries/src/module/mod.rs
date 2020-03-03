bbff5ef640ae8ccf8387d7645db5652242010956
Dexter Chua <dalcde@yahoo.com.hk>
2020-03-02 19:41:08 -0500

    impl Module for Box<dyn Module>

--- libraries/src/module/mod.rs
+++ libraries/src/module/mod.rs
@@ -214,6 +214,104 @@ pub trait Module: Send + Sync + 'static {
     }
 }
 
+impl<A: Algebra> Module for Box<dyn Module<Algebra = A>> {
+    type Algebra = A;
+
+    fn algebra(&self) -> Arc<Self::Algebra> {
+        (&**self).algebra()
+    }
+
+    fn name(&self) -> &str {
+        (&**self).name()
+    }
+
+    fn min_degree(&self) -> i32 {
+        (&**self).min_degree()
+    }
+
+    fn compute_basis(&self, degree: i32) {
+        (&**self).compute_basis(degree);
+    }
+    fn dimension(&self, degree: i32) -> usize {
+        (&**self).dimension(degree)
+    }
+
+    fn act_on_basis(
+        &self,
+        result: &mut FpVector,
+        coeff: u32,
+        op_degree: i32,
+        op_index: usize,
+        mod_degree: i32,
+        mod_index: usize,
+    ) {
+        (&**self).act_on_basis(result, coeff, op_degree, op_index, mod_degree, mod_index);
+    }
+
+    fn basis_element_to_string(&self, degree: i32, idx: usize) -> String {
+        (&**self).basis_element_to_string(degree, idx)
+    }
+
+    // Whether this is the unit module.
+    fn is_unit(&self) -> bool {
+        (&**self).is_unit()
+    }
+
+    fn prime(&self) -> ValidPrime {
+        (&**self).prime()
+    }
+
+    /// Whether act_on_basis_borrow is available.
+    fn borrow_output(&self) -> bool {
+        (&**self).borrow_output()
+    }
+
+    /// Returns a borrow of the value of the corresponding action on the basis element. This
+    /// FpVector must be "pure", i.e. it is not sliced and the limbs are zero in indices greater
+    /// than the dimension of the vector.
+    fn act_on_basis_borrow(
+        &self,
+        op_degree: i32,
+        op_index: usize,
+        mod_degree: i32,
+        mod_index: usize,
+    ) -> &FpVector {
+        (&**self).act_on_basis_borrow(op_degree, op_index, mod_degree, mod_index)
+    }
+
+    fn act(
+        &self,
+        result: &mut FpVector,
+        coeff: u32,
+        op_degree: i32,
+        op_index: usize,
+        input_degree: i32,
+        input: &FpVector,
+    ) {
+        (&**self).act(result, coeff, op_degree, op_index, input_degree, input);
+    }
+
+    fn act_by_element(
+        &self,
+        result: &mut FpVector,
+        coeff: u32,
+        op_degree: i32,
+        op: &FpVector,
+        input_degree: i32,
+        input: &FpVector,
+    ) {
+        (&**self).act_by_element(result, coeff, op_degree, op, input_degree, input);
+    }
+
+    fn generator_list_string(&self, degree: i32) -> String {
+        (&**self).generator_list_string(degree)
+    }
+
+    fn element_to_string(&self, degree: i32, element: &FpVector) -> String {
+        (&**self).element_to_string(degree, element)
+    }
+}
+
 // Poor man's trait alias
 pub trait SteenrodModule: Module<Algebra = SteenrodAlgebra> {}
 impl<M: Module<Algebra = SteenrodAlgebra>> SteenrodModule for M {}
