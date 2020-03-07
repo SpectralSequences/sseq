23ebced7ccd248edcd2bcad9b367eaabb7c75dc4
Hood <hood@mit.edu>
2020-03-07 12:43:49 -0500

    More python stuff.

---
+++ python/pytest/test.py
@@ -0,0 +1,13 @@
+import pytest
+
+class TestPrime:
+    def is_prime(self, n):
+        return n in [2,3,5,7,11,13,17,19,23,29]
+
+    def setup_class(self):
+        self.pg = 1    
+
+    @pytest.mark.parametrize("n, expected", [[17, True], [19, True],[21, False]])
+    def test_isprime(self, n, expected):
+        print(self.pg)
+        assert self.is_prime(n) == expected        
