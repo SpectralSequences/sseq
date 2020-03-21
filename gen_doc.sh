8c5a1a87f07b56ccf0d21d0df4d0e976e4519f35
Hood <hood@mit.edu>
2020-03-21 14:33:11 -0400

    Rearranged some stuff.

--- gen_doc.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-RUSTDOCFLAGS="--html-in-header katex-header.html" cargo doc --all --no-deps --document-private-items $1
--- python/pyo3/python_algebra/src/module/homomorphism/mod.rs
+++
@@ -1,7 +0,0 @@
-mod free_module_homomorphism;
-mod module_homomorphism_rust;
-
-pub use free_module_homomorphism::*;
-pub use module_homomorphism_rust::*;
-
-// pub use free_module_homomorphism::FreeModuleHomomorphism;
