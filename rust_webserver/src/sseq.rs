SpectralSequences/sseq

commit 7cf2c5e8c922a3cf5736c2230393000425957b3f

rust_webserver: Move more logic away from sseq.rs
This is leading towards a more solid Subquotient object that can be used
in future versions of sseq handling code

rust_webserver/src/sseq.rs
@@ -444,21 +444,18 @@ impl Sseq {
                     // Compute the ones where something changes.
                     for r in MIN_PAGE + 1 .. max_page {
                         let source_data = Sseq::get_page(r, &self.page_data[x][y]);
-                        let target_data = Sseq::get_page(r, &self.page_data[x + mult.x][y + mult.y]);
-
-                        let mut result = Vec::with_capacity(source_data.dimension());
-                        let mut target = FpVector::new(self.p, target_dim);
-                        for vec in source_data.gens() {
-                            matrix.apply(&mut target, 1, vec);
-                            result.push(target_data.reduce(&mut target));
-                            target.set_to_zero_pure();
-                        }
-                        matrices.push(result);
-
                         if source_data.is_empty() {
                             break;
                         }
+                        let target_data = Sseq::get_page(r, &self.page_data[x + mult.x][y + mult.y]);
+
+                        matrices.push(Subquotient::reduce_matrix(
+                            &matrix,
+                            source_data,
+                            target_data
+                        ));
                     }
+
                     structlines.push(ProductItem {
                         name : mult.name.clone(),
                         mult_x : mult.x,
@@ -1046,12 +1043,12 @@ impl Sseq {
 
         for (x, data) in self.page_data.iter_enum() {
             for (y, data) in data.iter_enum() {
-                let target_data = Sseq::get_page(r, data);
-                if target_data.is_empty() {
+                let data = Sseq::get_page(r, data);
+                if data.is_empty() {
                     continue;
                 }
 
-                g.node(x, y, target_data.dimension())?;
+                g.node(x, y, data.dimension())?;
 
                 // Now add the products hitting this bidegree
                 for &prod_name in products {
@@ -1070,19 +1067,8 @@ impl Sseq {
                     }
 
                     let matrix = prod.matrices[source_x][source_y].as_ref().unwrap();
-                    let mut value = FpVector::new(self.p, self.classes[x][y]);
-
-                    for (k, vec) in source_data.gens().enumerate() {
-                        matrix.apply(&mut value, 1, vec);
-                        let result = target_data.reduce(&mut value);
-                        for (i, v) in result.iter().enumerate() {
-                            if *v == 0 {
-                                continue;
-                            }
-                            g.structline((source_x, source_y, k), (x, y, i), None)?;
-                        }
-                        value.set_to_zero_pure();
-                    }
+                    let matrix = Subquotient::reduce_matrix(&matrix, source_data, data);
+                    g.structline_matrix((source_x, source_y), (x, y), matrix, None)?;
 
                     // Finally add the differentials
                     if differentials {

ext/crates/chart/src/lib.rs
@@ -184,6 +184,23 @@ impl<T: Write> Graph<T> {
         Ok(())
     }
 
+    pub fn structline_matrix(
+        &mut self,
+        source: (i32, i32),
+        target: (i32, i32),
+        matrix: Vec<Vec<u32>>,
+        class: Option<&str>,
+    ) -> Result<()> {
+        for (k, row) in matrix.into_iter().enumerate() {
+            for (l, v) in row.into_iter().enumerate() {
+                if v != 0 {
+                    self.structline((source.0, source.1, k), (target.0, target.1, l), class)?;
+                }
+            }
+        }
+        Ok(())
+    }
+
     /// Print the legend for node patterns
     pub fn legend(mut out: T) -> Result<()> {
         writeln!(

ext/crates/fp/src/matrix/subquotient.rs
@@ -1,4 +1,5 @@
 use super::Subspace;
+use crate::matrix::Matrix;
 use crate::prime::ValidPrime;
 use crate::vector::{FpVector, FpVectorT};
 
@@ -84,6 +85,14 @@ impl Subquotient {
         self.dimension
     }
 
+    pub fn ambient_dimension(&self) -> usize {
+        self.gens.ambient_dimension()
+    }
+
+    pub fn prime(&self) -> ValidPrime {
+        self.gens.prime()
+    }
+
     pub fn is_empty(&self) -> bool {
         self.dimension == 0
     }
@@ -99,6 +108,17 @@ impl Subquotient {
         self.quotient.reduce(new_row);
         self.gens.row_reduce();
         self.dimension = self.gens.dimension();
+    }
+
+    pub fn reduce_matrix(matrix: &Matrix, source: &Self, target: &Self) -> Vec<Vec<u32>> {
+        let mut result = Vec::with_capacity(source.dimension());
+        let mut temp = FpVector::new(source.prime(), target.ambient_dimension());
+        for v in source.gens() {
+            matrix.apply(&mut temp, 1, v);
+            result.push(target.reduce(&mut temp));
+            temp.set_to_zero_pure()
+        }
+        result
     }
 
     /// Given a chain of subspaces `subspace` < `space` < k^`ambient_dimension`, compute the

ext/crates/fp/src/matrix/subspace.rs
@@ -170,6 +170,10 @@ impl Subspace {
             .unwrap_or(0)
     }
 
+    pub fn ambient_dimension(&self) -> usize {
+        self.matrix.columns()
+    }
+
     /// Returns a basis of the subspace.
     pub fn basis(&self) -> &[FpVector] {
         &self.matrix.vectors[..self.dimension()]

commit da66a052a9bbc1e5b8a57cdcf2fdc9c38b8c8b3a

Fix bug introduced in 7cf2c5e

rust_webserver/src/sseq.rs
@@ -444,9 +444,6 @@ impl Sseq {
                     // Compute the ones where something changes.
                     for r in MIN_PAGE + 1 .. max_page {
                         let source_data = Sseq::get_page(r, &self.page_data[x][y]);
-                        if source_data.is_empty() {
-                            break;
-                        }
                         let target_data = Sseq::get_page(r, &self.page_data[x + mult.x][y + mult.y]);
 
                         matrices.push(Subquotient::reduce_matrix(
@@ -454,6 +451,12 @@ impl Sseq {
                             source_data,
                             target_data
                         ));
+
+                        // In the case where the source is empty, we still want one empty array to
+                        // indicate that no structlines should be drawn from this page on.
+                        if source_data.is_empty() {
+                            break;
+                        }
                     }
 
                     structlines.push(ProductItem {