518559cac69d92c3cedf40d84fe7380dd2b39565
Dexter Chua <dec41@srcf.net>
2021-03-15 08:36:24 +0800

    rustfmt bivec

--- ext/crates/bivec/src/lib.rs
+++ ext/crates/bivec/src/lib.rs
@@ -1,9 +1,8 @@
-#![cfg_attr(rustfmt, rustfmt_skip)]
 use core::ops::Index;
 use core::ops::IndexMut;
-use std::slice::{Iter, IterMut};
+use serde::{Deserialize, Deserializer, Serialize, Serializer};
 use std::fmt;
-use serde::{Serialize, Serializer, Deserialize, Deserializer};
+use std::slice::{Iter, IterMut};
 
 /// A BiVec is like a Vec, except we allow indices to be negative. It has a min_degree
 /// property which tells us where the starting index is.
@@ -13,8 +12,8 @@ use serde::{Serialize, Serializer, Deserialize, Deserializer};
 /// v[1], v[2]` but not `v[3]`.
 #[derive(Clone, PartialEq, Eq)]
 pub struct BiVec<T> {
-    pub data : Vec<T>,
-    min_degree : i32
+    pub data: Vec<T>,
+    min_degree: i32,
 }
 
 impl<T: fmt::Debug> fmt::Debug for BiVec<T> {
@@ -31,29 +30,26 @@ impl<T> std::default::Default for BiVec<T> {
 }
 
 impl<T> BiVec<T> {
-    pub fn new(min_degree : i32) -> Self {
+    pub fn new(min_degree: i32) -> Self {
         Self {
-            data : Vec::new(),
-            min_degree
+            data: Vec::new(),
+            min_degree,
         }
     }
 
-    pub fn from_vec(min_degree : i32, data : Vec<T>) -> Self {
-        Self {
-            data,
-            min_degree
-        }
+    pub fn from_vec(min_degree: i32, data: Vec<T>) -> Self {
+        Self { data, min_degree }
     }
 
     pub fn into_vec(self: BiVec<T>) -> Vec<T> {
         self.data
     }
 
-    pub fn with_capacity(min_degree : i32, capacity : i32) -> Self {
+    pub fn with_capacity(min_degree: i32, capacity: i32) -> Self {
         debug_assert!(capacity >= min_degree);
         Self {
-            data : Vec::with_capacity((capacity - min_degree) as usize),
-            min_degree
+            data: Vec::with_capacity((capacity - min_degree) as usize),
+            min_degree,
         }
     }
 
@@ -91,11 +87,11 @@ impl<T> BiVec<T> {
         self.data.is_empty()
     }
 
-    pub fn push(&mut self, x : T) {
+    pub fn push(&mut self, x: T) {
         self.data.push(x);
     }
 
-    pub fn get(&self, idx : i32) -> Option<&T> {
+    pub fn get(&self, idx: i32) -> Option<&T> {
         self.data.get((idx - self.min_degree) as usize)
     }
 
@@ -112,29 +108,34 @@ impl<T> BiVec<T> {
 
     pub fn iter_enum(&self) -> impl DoubleEndedIterator<Item = (i32, &T)> {
         let min_degree = self.min_degree;
-        self.data.iter().enumerate()
+        self.data
+            .iter()
+            .enumerate()
             .map(move |(i, t)| (i as i32 + min_degree, t))
-            // .collect()
+        // .collect()
     }
 
     pub fn into_iter_enum(self) -> impl DoubleEndedIterator<Item = (i32, T)> {
         let min_degree = self.min_degree;
-        self.data.into_iter().enumerate()
+        self.data
+            .into_iter()
+            .enumerate()
             .map(move |(i, t)| (i as i32 + min_degree, t))
-            // .collect()
+        // .collect()
     }
 
     /// Extends the bivec such that `max_degree()` is at least `max`. If `max_degree()` is
     /// already at least `max`, the function does nothing. Otherwise, it fills the new entries
     /// with the return value of `F(i)`, where i is the index of the new entry.
-    pub fn extend_with<F>(&mut self, max : i32, mut f : F)
-        where F : FnMut(i32) -> T
+    pub fn extend_with<F>(&mut self, max: i32, mut f: F)
+    where
+        F: FnMut(i32) -> T,
     {
         if max > self.max_degree() {
-             self.data.reserve((max - self.max_degree()) as usize);
-             for i in self.len() ..= max {
-                 self.data.push(f(i));
-             }
+            self.data.reserve((max - self.max_degree()) as usize);
+            for i in self.len()..=max {
+                self.data.push(f(i));
+            }
         }
     }
 
@@ -169,17 +170,19 @@ impl<T> BiVec<T> {
     }
 }
 
-impl<T : Serialize> Serialize for BiVec<T> {
+impl<T: Serialize> Serialize for BiVec<T> {
     fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
-        where S : Serializer
+    where
+        S: Serializer,
     {
         self.data.serialize(serializer) // Do better than this
     }
 }
 
-impl<'de, T : Deserialize<'de>> Deserialize<'de> for BiVec<T> {
+impl<'de, T: Deserialize<'de>> Deserialize<'de> for BiVec<T> {
     fn deserialize<D>(_deserializer: D) -> Result<Self, D::Error>
-        where D : Deserializer<'de>
+    where
+        D: Deserializer<'de>,
     {
         unimplemented!()
     }
@@ -187,30 +190,30 @@ impl<'de, T : Deserialize<'de>> Deserialize<'de> for BiVec<T> {
 
 impl<T> Index<i32> for BiVec<T> {
     type Output = T;
-    fn index(&self, i : i32) -> &T {
+    fn index(&self, i: i32) -> &T {
         &(self.data[(i - self.min_degree) as usize])
     }
 }
 
 impl<T> IndexMut<i32> for BiVec<T> {
-    fn index_mut(&mut self, i : i32) -> &mut T {
+    fn index_mut(&mut self, i: i32) -> &mut T {
         &mut (self.data[(i - self.min_degree) as usize])
     }
 }
+use saveload::{Load, Save};
 use std::io;
 use std::io::{Read, Write};
-use saveload::{Save, Load};
 
-impl<T : Save> Save for BiVec<T> {
-    fn save(&self, buffer : &mut impl Write) -> io::Result<()> {
+impl<T: Save> Save for BiVec<T> {
+    fn save(&self, buffer: &mut impl Write) -> io::Result<()> {
         self.data.save(buffer)
     }
 }
 
-impl<T : Load> Load for BiVec<T> {
+impl<T: Load> Load for BiVec<T> {
     type AuxData = (i32, T::AuxData);
 
-    fn load(buffer : &mut impl Read, data : &Self::AuxData) -> io::Result<Self> {
+    fn load(buffer: &mut impl Read, data: &Self::AuxData) -> io::Result<Self> {
         let min_degree = data.0;
         let data = Load::load(buffer, &data.1)?;
 
