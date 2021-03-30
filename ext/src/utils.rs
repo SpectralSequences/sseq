1576a0242ed0466e85806496ba3e70afd500a5cd
Dexter Chua <dec41@srcf.net>
2021-03-30 17:19:16 +0800

    rustfmt utils.rs and clean up

--- ext/src/utils.rs
+++ ext/src/utils.rs
@@ -1,22 +1,20 @@
-#![cfg_attr(rustfmt, rustfmt_skip)]
-use std::error::Error;
-use std::sync::Arc;
+use crate::chain_complex::{FiniteChainComplex, FreeChainComplex};
+use crate::resolution::Resolution;
+use crate::CCC;
+use algebra::module::FiniteModule;
+use algebra::{Algebra, SteenrodAlgebra};
+use fp::matrix::Matrix;
 use serde_json::{json, Value};
 
+use std::error::Error;
 use std::path::PathBuf;
-use algebra::{Algebra, SteenrodAlgebra};
-use algebra::module::FiniteModule;
-use fp::matrix::Matrix;
-use crate::chain_complex::{FreeChainComplex, FiniteChainComplex};
-use crate::resolution::Resolution;
-
-use crate::CCC;
+use std::sync::Arc;
 
 pub struct Config {
-    pub module_paths : Vec<PathBuf>,
-    pub module_file_name : String,
-    pub algebra_name : String,
-    pub max_degree : i32
+    pub module_paths: Vec<PathBuf>,
+    pub module_file_name: String,
+    pub algebra_name: String,
+    pub max_degree: i32,
 }
 
 pub fn get_config() -> Config {
@@ -32,20 +30,26 @@ pub fn get_config() -> Config {
 
     Config {
         module_paths: vec![current_dir, static_modules_path],
-        algebra_name: args.opt_value_from_str("--algebra").unwrap().unwrap_or_else(|| "adem".into()),
-        module_file_name: args.opt_free_from_str().unwrap().unwrap_or_else(|| "S_2".into()),
+        algebra_name: args
+            .opt_value_from_str("--algebra")
+            .unwrap()
+            .unwrap_or_else(|| "adem".into()),
+        module_file_name: args
+            .opt_free_from_str()
+            .unwrap()
+            .unwrap_or_else(|| "S_2".into()),
         max_degree: args.opt_free_from_str().unwrap().unwrap_or(30),
     }
 }
 
-pub fn construct(config : &Config) -> error::Result<Resolution<CCC>> {
+pub fn construct(config: &Config) -> error::Result<Resolution<CCC>> {
     let contents = load_module_from_file(config)?;
     let json = serde_json::from_str(&contents)?;
 
     construct_from_json(json, &config.algebra_name)
 }
 
-pub fn construct_from_json(mut json : Value, algebra_name : &str) -> error::Result<Resolution<CCC>> {
+pub fn construct_from_json(mut json: Value, algebra_name: &str) -> error::Result<Resolution<CCC>> {
     let algebra = Arc::new(SteenrodAlgebra::from_json(&json, algebra_name)?);
     let module = Arc::new(FiniteModule::from_json(Arc::clone(&algebra), &mut json)?);
     #[allow(unused_mut)] // This is only mut with Yoneda enabled
@@ -55,10 +59,10 @@ pub fn construct_from_json(mut json : Value, algebra_name : &str) -> error::Resu
     let cofiber = &json["cofiber"];
     #[cfg(feature = "yoneda")]
     if !cofiber.is_null() {
-        use crate::yoneda::yoneda_representative;
         use crate::chain_complex::ChainMap;
+        use crate::yoneda::yoneda_representative;
         use algebra::module::homomorphism::FreeModuleHomomorphism;
-        use algebra::module::{Module, BoundedModule};
+        use algebra::module::{BoundedModule, Module};
 
         let s = cofiber["s"].as_u64().unwrap() as u32;
         let t = cofiber["t"].as_i64().unwrap() as i32;
@@ -67,7 +71,11 @@ pub fn construct_from_json(mut json : Value, algebra_name : &str) -> error::Resu
         resolution.resolve_through_bidegree(s, t + module.max_degree());
 
         let map = FreeModuleHomomorphism::new(resolution.module(s), Arc::clone(&module), t);
-        let mut new_output = Matrix::new(module.prime(), resolution.module(s).number_of_gens_in_degree(t), 1);
+        let mut new_output = Matrix::new(
+            module.prime(),
+            resolution.module(s).number_of_gens_in_degree(t),
+            1,
+        );
         new_output[idx].set_entry(0, 1);
 
         let lock = map.lock();
@@ -76,8 +84,8 @@ pub fn construct_from_json(mut json : Value, algebra_name : &str) -> error::Resu
         map.extend_by_zero_safe(module.max_degree() + t);
 
         let cm = ChainMap {
-            s_shift : s,
-            chain_maps : vec![map]
+            s_shift: s,
+            chain_maps: vec![map],
         };
         let yoneda = yoneda_representative(Arc::clone(&resolution.inner), cm);
         let mut yoneda = FiniteChainComplex::from(yoneda);
@@ -98,7 +106,7 @@ pub fn construct_from_json(mut json : Value, algebra_name : &str) -> error::Resu
         for prod in products {
             let hom_deg = prod["hom_deg"].as_u64().unwrap() as u32;
             let int_deg = prod["int_deg"].as_i64().unwrap() as i32;
-            let class : Vec<u32> = serde_json::from_value(prod["class"].take()).unwrap();
+            let class: Vec<u32> = serde_json::from_value(prod["class"].take()).unwrap();
             let name = prod["name"].as_str().unwrap();
 
             resolution.add_product(hom_deg, int_deg, class, &name.to_string());
@@ -113,7 +121,7 @@ pub fn construct_from_json(mut json : Value, algebra_name : &str) -> error::Resu
             let name = self_map["name"].as_str().unwrap();
 
             let json_map_data = self_map["map_data"].as_array().unwrap();
-            let json_map_data : Vec<&Vec<Value>> = json_map_data
+            let json_map_data: Vec<&Vec<Value>> = json_map_data
                 .iter()
                 .map(|x| x.as_array().unwrap())
                 .collect();
@@ -132,7 +140,7 @@ pub fn construct_from_json(mut json : Value, algebra_name : &str) -> error::Resu
     Ok(resolution)
 }
 
-pub fn load_module_from_file(config : &Config) -> error::Result<String> {
+pub fn load_module_from_file(config: &Config) -> error::Result<String> {
     let mut result = None;
     for path in &config.module_paths {
         let mut path = path.clone();
@@ -143,9 +151,13 @@ pub fn load_module_from_file(config : &Config) -> error::Result<String> {
             break;
         }
     }
-    result.ok_or_else(|| ModuleFileNotFoundError {
-        name : config.module_file_name.clone()
-    }.into())
+    result.ok_or_else(|| {
+        error::GenericError::new(format!(
+            "Module file '{}' not found on path",
+            config.module_file_name
+        ))
+        .into()
+    })
 }
 
 pub fn construct_s_2(algebra: &str) -> Resolution<CCC> {
@@ -171,12 +183,12 @@ macro_rules! load_s_2 {
             resolution = ext::resolution::Resolution::load(&mut f, &resolution.complex()).unwrap();
         }
         let $resolution = resolution;
-    }
+    };
 }
 
 #[derive(Debug)]
 struct ModuleFileNotFoundError {
-    name : String
+    name: String,
 }
 
 impl std::fmt::Display for ModuleFileNotFoundError {
@@ -207,49 +219,66 @@ pub fn ascii_num(n: usize) -> char {
     }
 }
 
-pub fn print_resolution_color<C: FreeChainComplex, S: std::hash::BuildHasher>(res: &C, max_s: u32, max_t: i32, highlight: &std::collections::HashMap<(u32, i32), u32, S>) {
+pub fn print_resolution_color<C: FreeChainComplex, S: std::hash::BuildHasher>(
+    res: &C,
+    max_s: u32,
+    max_t: i32,
+    highlight: &std::collections::HashMap<(u32, i32), u32, S>,
+) {
     use std::io::Write;
     let stdout = std::io::stdout();
     let mut stdout = stdout.lock();
-    for s in (0 ..= max_s).rev() {
-        for t in s as i32 ..= max_t {
+    for s in (0..=max_s).rev() {
+        for t in s as i32..=max_t {
             if matches!(highlight.get(&(s, t)), None | Some(0)) {
-                write!(stdout, "{}{}{} ", RED_ANSI_CODE, ascii_num(res.module(s).number_of_gens_in_degree(t)), WHITE_ANSI_CODE).unwrap();
+                write!(
+                    stdout,
+                    "{}{}{} ",
+                    RED_ANSI_CODE,
+                    ascii_num(res.module(s).number_of_gens_in_degree(t)),
+                    WHITE_ANSI_CODE
+                )
+                .unwrap();
             } else {
-                write!(stdout, "{} ", ascii_num(res.module(s).number_of_gens_in_degree(t))).unwrap();
+                write!(
+                    stdout,
+                    "{} ",
+                    ascii_num(res.module(s).number_of_gens_in_degree(t))
+                )
+                .unwrap();
             }
         }
         writeln!(stdout).unwrap();
     }
 }
 
-use std::hash::{Hash, Hasher, BuildHasher};
 use std::collections::HashMap;
+use std::hash::{BuildHasher, Hash, Hasher};
 
 pub trait HashMapTuple<A, B, C> {
     fn get_tuple(&self, a: &A, b: &B) -> Option<&C>;
 }
 
-impl<A: Eq + Hash, B: Eq + Hash, C, S: BuildHasher> HashMapTuple<A, B, C> for HashMap<(A, B), C, S> {
+impl<A: Eq + Hash, B: Eq + Hash, C, S: BuildHasher> HashMapTuple<A, B, C>
+    for HashMap<(A, B), C, S>
+{
     fn get_tuple(&self, a: &A, b: &B) -> Option<&C> {
         let mut hasher = self.hasher().build_hasher();
         a.hash(&mut hasher);
         b.hash(&mut hasher);
         let raw_entry = self.raw_entry();
 
-        raw_entry.from_hash(
-            hasher.finish(),
-            |v| &v.0 == a && &v.1 == b,
-        ).map(|(_, y)| y)
+        raw_entry
+            .from_hash(hasher.finish(), |v| &v.0 == a && &v.1 == b)
+            .map(|(_, y)| y)
     }
 }
 
 /// Iterate through all pairs (s, f, t) such that f = t - s, s <= max_s and t <= max_t
-pub fn iter_stems(max_s: u32, max_t: i32) -> impl Iterator<Item=(u32, i32, i32)> {
+pub fn iter_stems(max_s: u32, max_t: i32) -> impl Iterator<Item = (u32, i32, i32)> {
     (0..=max_t)
         .map(move |f| {
-            (0..=std::cmp::min(max_s, (max_t - f) as u32))
-                .map(move |s| (s, f, f + s as i32))
+            (0..=std::cmp::min(max_s, (max_t - f) as u32)).map(move |s| (s, f, f + s as i32))
         })
         .flatten()
 }
@@ -266,6 +295,5 @@ mod test {
         assert_eq!(x.get_tuple(&5, &3), Some(&true));
         assert_eq!(x.get_tuple(&3, &5), None);
         assert_eq!(x.get_tuple(&7, &12), None);
-
     }
 }
