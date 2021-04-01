7fc88a2420673fb128a7355d048d5094e23354ee
Dexter Chua <dec41@srcf.net>
2021-04-01 20:46:08 +0800

    Add concurrent resolving benchmark

---
+++ ext/benches/resolve_concurrent.rs
@@ -0,0 +1,30 @@
+#[cfg(feature = "concurrent")]
+fn benchmark(algebra: &str) {
+    use ext::chain_complex::ChainComplex;
+    use ext::utils::construct_s_2;
+    use std::io::Write;
+    use std::time::Instant;
+    use thread_token::TokenBucket;
+
+    let resolution = construct_s_2::<&str>(algebra, None);
+    let bucket = TokenBucket::new(3);
+
+    print!("benchmark  {:6}  S_2  80:    ", algebra,);
+    std::io::stdout().flush().unwrap();
+
+    let start = Instant::now();
+    resolution.resolve_through_bidegree_concurrent(80, 80, &bucket);
+    let dur = start.elapsed();
+
+    assert!(resolution.module(80).number_of_gens_in_degree(80) < 1000);
+
+    println!("{} ms / iter", dur.as_millis());
+}
+
+#[cfg(not(feature = "concurrent"))]
+fn benchmark(_algebra: &str) {}
+
+fn main() {
+    benchmark("adem");
+    benchmark("milnor");
+}
