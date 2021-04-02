697654d4c4498902df75c058c26268cb28a1aab4
Dexter Chua <dec41@srcf.net>
2021-04-02 11:17:02 +0800

    Test resolve_through_stem save/load

--- ext/tests/save_load_resolution.rs
+++ ext/tests/save_load_resolution.rs
@@ -44,3 +44,35 @@ fn test_save_load() {
         resolution2.differential(5).quasi_inverse(7)
     );
 }
+
+#[test]
+fn test_save_load_stem() {
+    let mut json = json!({
+        "type": "finite dimensional module",
+        "p": 2,
+        "gens": {"x0": 0},
+        "actions": []
+    });
+
+    let resolution1 = construct_from_json(&mut json, "adem").unwrap();
+    resolution1.resolve_through_stem(10, 10);
+    resolution1.resolve_through_stem(10, 10);
+
+    let mut cursor: Cursor<Vec<u8>> = Cursor::new(Vec::new());
+    resolution1.save(&mut cursor).unwrap();
+
+    cursor.seek(SeekFrom::Start(0)).unwrap();
+
+    let resolution2 = Resolution::load(&mut cursor, &resolution1.complex()).unwrap();
+    assert_eq!(0, cursor.bytes().count());
+
+    assert_eq!(
+        resolution1.graded_dimension_string(),
+        resolution2.graded_dimension_string()
+    );
+
+    assert_eq!(
+        resolution1.differential(5).quasi_inverse(7),
+        resolution2.differential(5).quasi_inverse(7)
+    );
+}
