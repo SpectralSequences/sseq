ae87bfccc6d417116fc3d9527f7168cc8cee6f51
Dexter Chua <dec41@srcf.net>
2021-04-09 11:44:51 +0800

    Restore terminal state after printing resolution
    Avoid bolding later output

--- ext/examples/resolve.rs
+++ ext/examples/resolve.rs
@@ -19,6 +19,6 @@ fn main() -> error::Result<()> {
         res.resolve_through_bidegree_concurrent(max_degree as u32, max_degree, &bucket);
     }
 
-    println!("\x1b[1m{}", res.graded_dimension_string());
+    println!("\x1b[1m{}\x1b[0m", res.graded_dimension_string());
     Ok(())
 }
