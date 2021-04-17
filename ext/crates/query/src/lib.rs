d282a8e72de89879af9b00ed3fedeb3b60cfbc8b
Dexter Chua <dec41@srcf.net>
2021-04-17 14:19:24 +0800

    Avoid code duplication in query

--- ext/crates/query/src/lib.rs
+++ ext/crates/query/src/lib.rs
@@ -7,30 +7,35 @@ where
     F: Fn(T) -> Result<S, String>,
     <T as FromStr>::Err: Display,
 {
-    loop {
-        eprint!("{}: ", prompt);
-        stderr().flush().unwrap();
-        let mut input = String::new();
-        stdin()
-            .read_line(&mut input)
-            .unwrap_or_else(|_| panic!("Error reading for prompt: {}", prompt));
-        let trimmed = input.trim();
-        if trimmed.is_empty() {
-            return None;
+    inner(prompt, |x| {
+        if x.is_empty() {
+            Ok(None)
+        } else {
+            x.parse::<T>()
+                .map_err(|err| err.to_string())
+                .and_then(|res| validator(res))
+                .map(|x| Some(x))
         }
-        let result = trimmed
-            .parse::<T>()
-            .map_err(|err| format!("{}", err))
-            .and_then(|res| validator(res));
-        match result {
-            Ok(res) => {
-                return Some(res);
-            }
-            Err(e) => {
-                eprintln!("Invalid input: {}. Try again", e);
-            }
+    })
+}
+
+pub fn with_default<S: Display, T: FromStr, F>(prompt: &str, default: &str, validator: F) -> S
+where
+    F: Fn(T) -> Result<S, String>,
+    <T as FromStr>::Err: Display,
+{
+    inner(prompt, |x| {
+        if x.is_empty() {
+            default
+                .parse::<T>()
+                .map_err(|err| err.to_string())
+                .and_then(|res| validator(res))
+        } else {
+            x.parse::<T>()
+                .map_err(|err| err.to_string())
+                .and_then(|res| validator(res))
         }
-    }
+    })
 }
 
 pub fn raw<S: Display, T: FromStr, F>(prompt: &str, validator: F) -> S
@@ -38,58 +43,11 @@ where
     F: Fn(T) -> Result<S, String>,
     <T as FromStr>::Err: Display,
 {
-    loop {
-        eprint!("{} : ", prompt);
-        stderr().flush().unwrap();
-        let mut input = String::new();
-        stdin()
-            .read_line(&mut input)
-            .unwrap_or_else(|_| panic!("Error reading for prompt: {}", prompt));
-        let trimmed = input.trim();
-        let result = trimmed
-            .parse::<T>()
-            .map_err(|err| format!("{}", err))
-            .and_then(|res| validator(res));
-        match result {
-            Ok(res) => {
-                return res;
-            }
-            Err(e) => {
-                eprintln!("Invalid input: {}. Try again", e);
-            }
-        }
-    }
-}
-
-pub fn with_default<S: Display, T: FromStr, F>(prompt: &str, default: &str, validator: F) -> S
-where
-    F: Fn(T) -> Result<S, String>,
-    <T as std::str::FromStr>::Err: std::fmt::Display,
-{
-    loop {
-        eprint!("{} (default: {}): ", prompt, default);
-        stderr().flush().unwrap();
-        let mut input = String::new();
-        stdin()
-            .read_line(&mut input)
-            .unwrap_or_else(|_| panic!("Error reading for prompt: {}", prompt));
-        let mut trimmed = input.trim();
-        if trimmed.is_empty() {
-            trimmed = default;
-        }
-        let result = trimmed
-            .parse::<T>()
-            .map_err(|err| format!("{}", err))
-            .and_then(|res| validator(res));
-        match result {
-            Ok(res) => {
-                return res;
-            }
-            Err(e) => {
-                eprintln!("Invalid input: {}. Try again", e);
-            }
-        }
-    }
+    inner(prompt, |x| {
+        x.parse::<T>()
+            .map_err(|err| err.to_string())
+            .and_then(|res| validator(res))
+    })
 }
 
 pub fn yes_no(prompt: &str) -> bool {
@@ -103,4 +61,27 @@ pub fn yes_no(prompt: &str) -> bool {
             ))
         }
     })
+}
+
+pub fn inner<S, F>(prompt: &str, validator: F) -> S
+where
+    F: for<'a> Fn(&'a str) -> Result<S, String>,
+{
+    loop {
+        eprint!("{} : ", prompt);
+        stderr().flush().unwrap();
+        let mut input = String::new();
+        stdin()
+            .read_line(&mut input)
+            .unwrap_or_else(|_| panic!("Error reading for prompt: {}", prompt));
+        let trimmed = input.trim();
+        match validator(trimmed) {
+            Ok(res) => {
+                return res;
+            }
+            Err(e) => {
+                eprintln!("Invalid input: {}. Try again", e);
+            }
+        }
+    }
 }
