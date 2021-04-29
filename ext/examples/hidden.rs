d02912d0989802e8264f26c4533e60688361cf11
Dexter Chua <dec41@srcf.net>
2021-04-29 21:37:12 +0800

    Add more documentation

--- ext/examples/hidden.rs
+++ ext/examples/hidden.rs
@@ -1,4 +1,29 @@
 //! Compute hidden α-extensions using precomputed tables. This assumes prime 2.
+//!
+//! # Usage guide
+//! We use X to denote the spectrum whose hidden extensions we want to compute, and α the class we
+//! multiply with.
+//!
+//! # Arguments
+//! We clarify some less obvious arguments:
+//!
+//!  - **Name of α:** See "α products"
+//!
+//!  - **Dimension of Ext of X:** The output of the `num_gens` example on X
+//!
+//!  - **α products:** A file containing lines of the form `α x_(n, s, i) = [...]`, where the first
+//!    word of each line is the name of the class we multiply with. The file can contain products
+//!    with different elements, and the "Name of α" argument is used to filter out the relevant
+//!    lines.
+//!
+//!    In existing applications, we use the output of `filtration_one` on X.
+//!
+//!  - **Inclusion map:** The output of `lift_hom` on the inclusion map of the bottom cell X -> X/α
+//!
+//!  - **Projection map:** The output of `lift_hom` on the projection map of the top cell X/α -> X
+//!
+//!  - **d2 of X:** The output of `secondary` on X.
+
 #![allow(clippy::redundant_closure)]
 
 use fp::matrix::AugmentedMatrix;
