8b20b0e625d9cc3eaba8ac13f2cb727cfee3accd
Dexter Chua <dec41@srcf.net>
2021-12-30 22:02:20 -0500

    sseq_gui: Test loading from json

--- web_ext/sseq_gui/tests/test_load.py
+++ web_ext/sseq_gui/tests/test_load.py
@@ -1,9 +1,21 @@
 import pytest
+from pathlib import Path
 
 
 @pytest.mark.parametrize("module", ["S_2", "S_3", "C2v14"])
 def test_load(driver, module: str):
     driver.go("/")
     driver.driver.find_element_by_css_selector(f'a[data="{module}"]').click()
+    driver.wait_complete()
+    driver.check_svg(f"{module}_load.svg")
+
+
+@pytest.mark.parametrize("module", ["S_2", "S_3", "C2v14"])
+def test_load_json(driver, module: str):
+    path = Path(__file__).parent / "../../../ext/steenrod_modules" / f"{module}.json"
+
+    driver.go("/")
+    driver.driver.find_element_by_id("json-upload").send_keys(str(path))
+    driver.reply("40")
     driver.wait_complete()
     driver.check_svg(f"{module}_load.svg")
