1175bac4e936a8dc25103e5965f1be6700736558
Dexter Chua <dec41@srcf.net>
2021-12-30 22:13:25 -0500

    Fix chrome tests

--- web_ext/sseq_gui/tests/test_load.py
+++ web_ext/sseq_gui/tests/test_load.py
@@ -13,6 +13,7 @@ def test_load(driver, module: str):
 @pytest.mark.parametrize("module", ["S_2", "S_3", "C2v14"])
 def test_load_json(driver, module: str):
     path = Path(__file__).parent / "../../../ext/steenrod_modules" / f"{module}.json"
+    path = path.resolve()
 
     driver.go("/")
     driver.driver.find_element_by_id("json-upload").send_keys(str(path))
