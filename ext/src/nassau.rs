13efff4d78c958259c2cd2ff3beded438c64bd58
Dexter Chua <dec41@srcf.net>
2022-01-09 14:42:43 -0500

    nassau: improve concurrency

--- ext/src/nassau.rs
+++ ext/src/nassau.rs
@@ -14,6 +14,30 @@ use fp::vector::FpVector;
 use itertools::Itertools;
 use once::OnceVec;
 
+#[cfg(feature = "concurrent")]
+use std::sync::mpsc;
+
+/// See [`Resolution::SenderData`]. This differs by not having the `new` field.
+#[cfg(feature = "concurrent")]
+struct SenderData {
+    s: u32,
+    t: i32,
+    sender: mpsc::Sender<SenderData>,
+}
+
+#[cfg(feature = "concurrent")]
+impl SenderData {
+    pub(crate) fn send(s: u32, t: i32, sender: mpsc::Sender<Self>) {
+        sender
+            .send(Self {
+                s,
+                t,
+                sender: sender.clone(),
+            })
+            .unwrap()
+    }
+}
+
 /// A Milnor subalgebra to be used in [Nassau's algorithm](https://arxiv.org/abs/1910.04063). This
 /// is equipped with an ordering of the signature as in Lemma 2.4 of the paper.
 ///
@@ -392,6 +416,7 @@ impl Resolution {
         self.extend_through_degree(max_s);
         self.algebra().compute_basis(max_t);
 
+        #[cfg(not(feature = "concurrent"))]
         for t in 0..=max_t {
             let start_s = std::cmp::max(0, t - max_n) as u32;
             for s in start_s..=max_s {
@@ -401,6 +426,54 @@ impl Resolution {
                 self.step_resolution(s, t);
             }
         }
+
+        #[cfg(feature = "concurrent")]
+        rayon::in_place_scope(|scope| {
+            // This algorithm is not optimal, as we compute (s, t) only after computing (s - 1, t)
+            // and (s, t - 1). In theory, it suffices to wait for (s, t - 1) and (s - 1, t - 1),
+            // but having the dimensions of the modules change halfway through the computation is
+            // annoying to do correctly. It seems more prudent to improve parallelism elsewhere.
+
+            // Things that we have finished computing.
+            let mut progress: Vec<i32> = vec![-1; max_s as usize + 1];
+            // We will kickstart the process by pretending we have computed (0, - 1). So
+            // we must pretend we have only computed up to (0, - 2);
+            progress[0] = -2;
+
+            let (sender, receiver) = mpsc::channel();
+            SenderData::send(0, -1, sender);
+
+            let f = |s, t, sender| {
+                if self.has_computed_bidegree(s, t) {
+                    SenderData::send(s, t, sender);
+                } else {
+                    let sender = sender.clone();
+                    scope.spawn(move |_| {
+                        self.step_resolution(s, t);
+                        SenderData::send(s, t, sender);
+                    });
+                }
+            };
+
+            while let Ok(SenderData { s, t, sender }) = receiver.recv() {
+                assert!(progress[s as usize] == t - 1);
+                progress[s as usize] = t;
+
+                // How far we are from the last one for this s.
+                let distance = max_n + 1 - (t - s as i32);
+
+                if s < max_s && progress[s as usize + 1] == t - 1 {
+                    f(s + 1, t, sender.clone());
+                }
+
+                if distance > 1 && (s == 0 || progress[s as usize - 1] > t) {
+                    // We are computing a normal step
+                    f(s, t + 1, sender);
+                } else if distance == 1 && s < max_s {
+                    SenderData::send(s, t + 1, sender);
+                }
+            }
+        });
     }
 }
 
