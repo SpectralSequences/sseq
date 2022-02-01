66937821b6c34e1115413f3c2216f8d6d94e5d00
Hood Chatham <roberthoodchatham@gmail.com>
2022-02-01 11:30:35 -0800

    Fix color deserialization

--- chart/chart/javascript/src/Color.ts
+++ chart/chart/javascript/src/Color.ts
@@ -2,6 +2,9 @@ export type Color = [number, number, number, number];
 export type DashPattern = number[];
 
 export function hexStringToColor(s : string) : Color {
-    let hexs = [s.slice(1,3), s.slice(3,5), s.slice(5,7)];
+    let hexs = [s.slice(1,3), s.slice(3,5), s.slice(5,7), "0xff"];
+    if(s.length === 9){
+        hexs[3] = s.slice(7, 9);
+    }
     return <Color>hexs.map(s => Number.parseInt(s, 16)/255);
 }
