e222b44eed879c5e2360987ef35f4803861608db
Adam Nemecek <adamnemecek@gmail.com>
2024-04-23 13:51:44 -0700

    fixed build break

--- web_ext/steenrod_calculator/src/lib.rs
+++ web_ext/steenrod_calculator/src/lib.rs
@@ -6,7 +6,7 @@ pub struct SteenrodCalculator(SteenrodEvaluator);
 
 #[wasm_bindgen]
 impl SteenrodCalculator {
-    pub fn new(p: u32) -> Option<Self> {
+    pub fn new(p: u32) -> Option<SteenrodCalculator> {
         Some(Self(SteenrodEvaluator::new(p.try_into().ok()?)))
     }
 
