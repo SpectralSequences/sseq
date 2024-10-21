c978f033d937ad0a581147606d97ae2453bae395
Joey Beauvais-Feisthauer <joeybf@wayne.edu>
2024-10-21 00:45:50 -0400

    Slight reorder

--- ext/crates/fp/src/matrix/mod.rs
+++ ext/crates/fp/src/matrix/mod.rs
@@ -7,15 +7,16 @@ mod subspace;
 
 mod m4ri;
 
-// pub use basis::Basis;
-pub use affine::AffineSubspace;
-pub use matrix_inner::{AugmentedMatrix, Matrix, MatrixSliceMut};
-pub use quasi_inverse::QuasiInverse;
-pub use subquotient::Subquotient;
-pub use subspace::Subspace;
 #[cfg(feature = "proptest")]
 pub mod arbitrary {
     pub use super::{
         matrix_inner::arbitrary::*, subquotient::arbitrary::*, subspace::arbitrary::*,
     };
 }
+
+// pub use basis::Basis;
+pub use affine::AffineSubspace;
+pub use matrix_inner::{AugmentedMatrix, Matrix, MatrixSliceMut};
+pub use quasi_inverse::QuasiInverse;
+pub use subquotient::Subquotient;
+pub use subspace::Subspace;
