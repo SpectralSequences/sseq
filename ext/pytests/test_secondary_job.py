2f44ceff8e5da87e6443122ed55342705be870c7
Joey Beauvais-Feisthauer <joeybf@wayne.edu>
2025-01-25 16:14:04 -0500

    Adjust tests

--- ext/pytests/test_secondary_job.py
+++ ext/pytests/test_secondary_job.py
@@ -2,9 +2,9 @@ import subprocess
 
 
 def folder_pairs(folder):
+    filename_parts = (f.stem.split("_") for f in folder.glob("*"))
     return (
-        (int(split[0]), int(split[1]))
-        for split in (f.stem.split("_") for f in folder.glob("*"))
+        (int(split[0].replace("m", "-")), int(split[1])) for split in filename_parts
     )
 
 
@@ -34,11 +34,11 @@ def test_secondary_job(tmp_path, monkeypatch):
         "secondary_composites",
         "secondary_intermediates",
     ]:
-        for s, t in folder_pairs(tmp_path / folder):
-            assert t - s <= max_stems
+        for n, _ in folder_pairs(tmp_path / folder):
+            assert n <= max_stems
 
-    for s, _ in folder_pairs(tmp_path / "secondary_composites"):
+    for _, s in folder_pairs(tmp_path / "secondary_composites"):
         assert s == goal_stem
 
-    for s, _ in folder_pairs(tmp_path / "secondary_intermediates"):
+    for _, s in folder_pairs(tmp_path / "secondary_intermediates"):
         assert s == goal_stem + 1
