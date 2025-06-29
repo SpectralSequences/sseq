e2974d7c7891c67fcc72312aa2977dd86a9ce2a0
Joey Beauvais-Feisthauer <joeybf@wayne.edu>
2025-06-29 04:11:47 -0400

    Use flake-based dev shell instead of devenv

---
+++ flake.nix
@@ -0,0 +1,42 @@
+{
+  description = "sseq flake for monorepo";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    fenix = {
+      url = "github:nix-community/fenix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = {
+    nixpkgs,
+    fenix,
+    flake-utils,
+    ...
+  }: {
+    # Expose inputs to subflakes
+    inherit nixpkgs fenix flake-utils;
+
+    defaultPackages = flake-utils.lib.eachDefaultSystem (system: {
+      rustToolchain = fenix.packages.${system}.complete.withComponents [
+        "rustc"
+        "cargo"
+        "clippy"
+        "rustfmt"
+        "rust-analyzer"
+        "miri"
+        "llvm-tools-preview"
+      ];
+      devTools = let
+        pkgs = import nixpkgs {inherit system;};
+      in [
+        pkgs.git
+        pkgs.hyperfine
+        pkgs.binutils
+        pkgs.cargo-binutils
+      ];
+    });
+  };
+}
