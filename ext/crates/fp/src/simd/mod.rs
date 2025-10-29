e046f6f38b741017fb1a0d81c7e744c0c1b9ca47
Joey Beauvais-Feisthauer <joeybf@wayne.edu>
2025-10-29 19:13:38 -0400

    Disregard 32-bit x86 architectures

--- ext/crates/fp/src/simd/mod.rs
+++ ext/crates/fp/src/simd/mod.rs
@@ -7,7 +7,7 @@ mod x86_64;
 
 pub(crate) fn add_simd(target: &mut [Limb], source: &[Limb], min_limb: usize) {
     cfg_if::cfg_if! {
-        if #[cfg(any(target_arch = "x86", target_arch = "x86_64"))] {
+        if #[cfg(target_arch = "x86_64")] {
             x86_64::add_simd(target, source, min_limb)
         } else {
             generic::add_simd(target, source, min_limb)
