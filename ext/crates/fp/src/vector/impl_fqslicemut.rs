0f3c78957274cab25efa56fd080af9c952d1d96d
Joey Beauvais-Feisthauer <joeybf@wayne.edu>
2025-11-26 00:29:16 -0500

    Factor out `FqSliceMut::reduce_limbs`

--- ext/crates/fp/src/vector/impl_fqslicemut.rs
+++ ext/crates/fp/src/vector/impl_fqslicemut.rs
@@ -10,17 +10,6 @@ use crate::{
 };
 
 impl<F: Field> FqSliceMut<'_, F> {
-    fn reduce_limbs(&mut self) {
-        let fq = self.fq();
-        if fq.q() != 2 {
-            let limb_range = self.as_slice().limb_range();
-
-            for limb in self.limbs_mut()[limb_range].iter_mut() {
-                *limb = fq.reduce(*limb);
-            }
-        }
-    }
-
     pub fn scale(&mut self, c: FieldElement<F>) {
         assert_eq!(self.fq(), c.field());
         let fq = self.fq();
