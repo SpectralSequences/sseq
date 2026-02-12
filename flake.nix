794c576b94aab8b1b86c304674a0fcaa6a0c5e9f
Joey Beauvais-Feisthauer <joeybf@wayne.edu>
2026-02-12 17:45:49 -0500

    Use monthly fenix in flake

--- flake.nix
+++ flake.nix
@@ -5,7 +5,7 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     flake-utils.url = "github:numtide/flake-utils";
     fenix = {
-      url = "github:nix-community/fenix";
+      url = "github:nix-community/fenix/monthly";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
