0ce1d2b5888759a0aadbf38962707a0754822937
Hood Chatham <roberthoodchatham@gmail.com>
2026-05-01 11:28:32 -0700

    Remove existing

--- python_ext/webserver/bin/run.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-echo "Initializing SpectralSequences/webserver..."
-# This is really just here to activate the virtualenv before running python.
-export WORKING_DIRECTORY="$(pwd)"
-export BIN="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-export REPOSITORY_ROOT="$(dirname "$BIN")"
-source $BIN/virtualenv/bin/activate
-CMDLINE_ARGS="$@"
-python $BIN/_run.py $@
