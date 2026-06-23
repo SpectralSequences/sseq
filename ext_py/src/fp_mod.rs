e23b2eba89b802711554f1b4b94c49715f5b9d5e
Hood Chatham <roberthoodchatham@gmail.com>
2026-06-23 10:19:18 -0700

    Use int boundary for primes in ext_py

--- ext_py/src/fp_mod.rs
+++ ext_py/src/fp_mod.rs
@@ -4,92 +4,15 @@ use pyo3::prelude::*;
 #[pyo3(name = "fp")]
 pub mod fp_py {
     use fp::prime::{self, Binomial, Prime};
-    use pyo3::basic::CompareOp;
+    use pyo3::exceptions::PyValueError;
 
     use super::*;
 
-    #[pyclass(frozen)]
-    #[derive(Clone, Copy, Debug, PartialEq, Eq, Hash)]
-    pub struct ValidPrime(pub prime::ValidPrime);
-
-    impl From<prime::ValidPrime> for ValidPrime {
-        fn from(value: prime::ValidPrime) -> Self {
-            Self(value)
-        }
-    }
-
-    impl From<ValidPrime> for prime::ValidPrime {
-        fn from(value: ValidPrime) -> Self {
-            value.0
-        }
-    }
-
-    #[pymethods]
-    impl ValidPrime {
-        #[new]
-        pub fn new(p: u32) -> Self {
-            prime::ValidPrime::new(p).into()
-        }
-
-        #[staticmethod]
-        pub fn new_unchecked(p: u32) -> Self {
-            prime::ValidPrime::new_unchecked(p).into()
-        }
-
-        pub fn as_i32(&self) -> i32 {
-            self.0.as_i32()
-        }
-
-        pub fn as_u32(&self) -> u32 {
-            self.0.as_u32()
-        }
-
-        pub fn as_usize(&self) -> usize {
-            self.0.as_usize()
-        }
-
-        pub fn sum(&self, a: u32, b: u32) -> u32 {
-            self.0.sum(a, b)
-        }
-
-        pub fn product(&self, a: u32, b: u32) -> u32 {
-            self.0.product(a, b)
-        }
-
-        pub fn inverse(&self, k: u32) -> u32 {
-            self.0.inverse(k)
-        }
-
-        pub fn pow(&self, exp: u32) -> u32 {
-            self.0.pow(exp)
-        }
-
-        pub fn pow_mod(&self, b: u32, e: u32) -> u32 {
-            self.0.pow_mod(b, e)
-        }
-
-        fn __int__(&self) -> u32 {
-            self.0.as_u32()
-        }
-
-        fn __index__(&self) -> u32 {
-            self.0.as_u32()
-        }
-
-        fn __hash__(&self) -> u64 {
-            self.0.as_u32() as u64
-        }
-
-        fn __repr__(&self) -> String {
-            format!("ValidPrime({})", self.0)
-        }
-
-        fn __richcmp__(&self, other: Self, op: CompareOp) -> bool {
-            match op {
-                CompareOp::Eq => self.0 == other.0,
-                CompareOp::Ne => self.0 != other.0,
-                _ => false,
-            }
+    fn valid_prime(p: u32) -> PyResult<prime::ValidPrime> {
+        if prime::is_prime(p) {
+            Ok(prime::ValidPrime::new(p))
+        } else {
+            Err(PyValueError::new_err(format!("{p} is not prime")))
         }
     }
 
@@ -104,23 +27,23 @@ pub mod fp_py {
     }
 
     #[pyfunction]
-    pub fn logp(p: ValidPrime, n: u32) -> u32 {
-        prime::logp(p.0, n)
+    pub fn logp(p: u32, n: u32) -> PyResult<u32> {
+        Ok(prime::logp(valid_prime(p)?, n))
     }
 
     #[pyfunction]
-    pub fn factor_pk(p: ValidPrime, n: u32) -> (u32, u32) {
-        prime::factor_pk(p.0, n)
+    pub fn factor_pk(p: u32, n: u32) -> PyResult<(u32, u32)> {
+        Ok(prime::factor_pk(valid_prime(p)?, n))
     }
 
     #[pyfunction]
-    pub fn inverse(p: ValidPrime, k: u32) -> u32 {
-        prime::inverse(p.0, k)
+    pub fn inverse(p: u32, k: u32) -> PyResult<u32> {
+        Ok(prime::inverse(valid_prime(p)?, k))
     }
 
     #[pyfunction]
-    pub fn minus_one_to_the_n(p: ValidPrime, i: i32) -> u32 {
-        prime::minus_one_to_the_n(p.0, i)
+    pub fn minus_one_to_the_n(p: u32, i: i32) -> PyResult<u32> {
+        Ok(prime::minus_one_to_the_n(valid_prime(p)?, i))
     }
 
     #[pyfunction]
@@ -129,18 +52,18 @@ pub mod fp_py {
     }
 
     #[pyfunction]
-    pub fn binomial(p: ValidPrime, n: u32, k: u32) -> u32 {
-        u32::binomial(p.0, n, k)
+    pub fn binomial(p: u32, n: u32, k: u32) -> PyResult<u32> {
+        Ok(u32::binomial(valid_prime(p)?, n, k))
     }
 
     #[pyfunction]
-    pub fn multinomial(p: ValidPrime, mut l: Vec<u32>) -> u32 {
-        u32::multinomial(p.0, &mut l)
+    pub fn multinomial(p: u32, mut l: Vec<u32>) -> PyResult<u32> {
+        Ok(u32::multinomial(valid_prime(p)?, &mut l))
     }
 
     #[pyfunction]
-    pub fn binomial_odd_is_zero(p: ValidPrime, n: u32, k: u32) -> bool {
-        u32::binomial_odd_is_zero(p.0, n, k)
+    pub fn binomial_odd_is_zero(p: u32, n: u32, k: u32) -> PyResult<bool> {
+        Ok(u32::binomial_odd_is_zero(valid_prime(p)?, n, k))
     }
 
     #[pyfunction]
@@ -165,7 +88,7 @@ pub mod fp_py {
 
     #[pymodule_init]
     fn init(m: &Bound<'_, PyModule>) -> PyResult<()> {
-        m.add("TWO", ValidPrime(prime::TWO))?;
+        m.add("TWO", prime::TWO.as_u32())?;
         m.add("PRIMES", fp::PRIMES.to_vec())?;
         m.add("NUM_PRIMES", fp::NUM_PRIMES)?;
         m.add("PRIME_TO_INDEX_MAP", fp::PRIME_TO_INDEX_MAP.to_vec())?;
